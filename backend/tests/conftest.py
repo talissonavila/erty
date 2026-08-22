be1592e8e9811153e7947f977b127fdcd5345fa3
Talisson Avila <devtalisson@gmail.com>
2026-08-21 22:21:36 -0300

    test: creating api test to health check route and creating a fixture to app

---
+++ backend/tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+@pytest.fixture
+def client():
+    with TestClient(app) as client:
+        yield client
